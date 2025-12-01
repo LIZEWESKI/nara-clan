face69b619aaca986359f2b01003fa43b5e5d779
Lizeweski <lilynoukh@gmail.com>
2025-12-01 20:55:36 +0100

    added brand logo

--- resources/js/components/app-logo-icon.jsx
+++ resources/js/components/app-logo-icon.jsx
@@ -1,12 +1,23 @@
 
-export default function AppLogoIcon(props) {
+export default function AppLogoIcon({size = 32}) {
     return (
-        <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z"
-            />
+        <svg width={size} height={size} viewBox="0 0 625 625" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clipPath="url(#clip0_16_134)">
+            <path d="M312.5 81L583.133 549.75H41.8671L312.5 81Z" fill="var(--foreground)" />
+            <g clipPath="url(#clip1_16_133)">
+            <line x1="214.586" y1="305.302" x2="410.586" y2="501.302" stroke="var(--background)" strokeWidth="40" />
+            <path d="M244 440L289.033 518H198.967L244 440Z" fill="var(--background)" />
+            <path d="M380 369L425.033 291H334.967L380 369Z" fill="var(--background)" />
+            </g>
+        </g>
+        <defs>
+            <clipPath id="clip0_16_133">
+            <rect width={size} height={size} fill="var(--background)" />
+            </clipPath>
+            <clipPath id="clip1_16_133">
+            <rect width="225" height="225" fill="var(--background)" transform="translate(200 291)" />
+            </clipPath>
+        </defs>
         </svg>
     );
 }
